21c6563bfde277061480c8c5a8c8c2a8362b981d
cremaster <25466659+cshape@users.noreply.github.com>
2023-04-23 00:56:41 -0700

    test route

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
  
 app = Flask(__name__)
  
 @app.route("/")
 def home_view():
         return "<h1>welcome to cale and hannahs hack projjjekt</h1>"
+
+@app.route("/test")
+def test():
+        return jsonify({'name': 'cale',
+                    'occupation': 'cool guy'})

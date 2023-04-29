c1c0e0e7eed79ae3996838a8c1dc9264fa3cedea
cremaster <25466659+cshape@users.noreply.github.com>
2023-04-29 11:25:37 -0700

    couple new route ideas

--- app/main.py
+++ app/main.py
@@ -1,12 +1,28 @@
 from flask import Flask, jsonify
  
 app = Flask(__name__)
+
+prompt_elements = []
+database_connection = [] # if long term
  
 @app.route("/")
 def home_view():
-        return "<h1>welcome to cale and hannahs hack projjjekt</h1>"
+        return "<h1>Hi Hannah!</h1><p>I wonder what we should name the project...</p><ul><li>Lift the Veil</li><li>Mary Maker</li></ul>
+        "
 
 @app.route("/test")
 def test():
         return jsonify({'name': 'cale',
                     'occupation': 'cool guy'})
+
+@app.route("/search")
+def search():
+        # takes some text as a param then searches google. returns google response
+
+@app.route("/openai")
+def openai():
+        # takes some text as a param then sends to gpt-3. returns gpt-3 response
+
+@app.route("/voiceflow")
+def voiceflow():
+        # takes some text as a param then sends a request to voiceflow. returns voiceflow response
